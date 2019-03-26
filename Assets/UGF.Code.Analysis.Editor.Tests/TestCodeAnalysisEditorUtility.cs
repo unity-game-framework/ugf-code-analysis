82d60f40bfd830433ed28360d01976964b1ff132
Alexander <vorobyovvorobyov@gmail.com>
2019-03-26 18:41:34 +0300

    Add Plugins

--- Assets/UGF.Code.Analysis.Editor.Tests/TestCodeAnalysisEditorUtility.cs
+++ Assets/UGF.Code.Analysis.Editor.Tests/TestCodeAnalysisEditorUtility.cs
@@ -1,6 +1,21 @@
-﻿namespace UGF.Code.Analysis.Editor.Tests
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using UnityEditor;
+
+namespace UGF.Code.Analysis.Editor.Tests
 {
     public class TestCodeAnalysisEditorUtility
     {
+        [Test]
+        public void Test()
+        {
+            string path = "Assets/UGF.Code.Analysis.Editor.Tests/TestCodeAnalysisEditorUtility.cs";
+            string text = AssetDatabase.LoadAssetAtPath<MonoScript>(path).text;
+            
+            SyntaxTree tree = SyntaxFactory.ParseSyntaxTree(text);
+            
+            Assert.NotNull(tree);
+        }
     }
 }
