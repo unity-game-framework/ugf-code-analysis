1aaa186ea9bd74a36640c62360b7235a9c4f026c
Alexander <vorobyovvorobyov@gmail.com>
2019-04-06 21:29:13 +0300

    Change CodeAnalysisEditorUtility.cs

--- Assets/UGF.Code.Analysis.Editor.Tests/TestCodeAnalysisEditorUtility.cs
+++ Assets/UGF.Code.Analysis.Editor.Tests/TestCodeAnalysisEditorUtility.cs
@@ -1,6 +1,55 @@
-﻿namespace UGF.Code.Analysis.Editor.Tests
+﻿using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Editing;
+using NUnit.Framework;
+
+namespace UGF.Code.Analysis.Editor.Tests
 {
     public class TestCodeAnalysisEditorUtility
     {
+        [Test]
+        public void ProjectCompilation()
+        {
+            CSharpCompilation result = CodeAnalysisEditorUtility.ProjectCompilation;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void Generator()
+        {
+            SyntaxGenerator result = CodeAnalysisEditorUtility.Generator;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void GetProjectCompilation()
+        {
+            CSharpCompilation result = CodeAnalysisEditorUtility.GetProjectCompilation();
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void GetSyntaxGenerator()
+        {
+            SyntaxGenerator result = CodeAnalysisEditorUtility.GetSyntaxGenerator();
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void PrintSyntaxTree()
+        {
+            string source = File.ReadAllText("Assets/UGF.Code.Analysis.Editor.Tests/TestTarget.txt");
+            string sourceTree = File.ReadAllText("Assets/UGF.Code.Analysis.Editor.Tests/TestTargetSyntaxTree.txt");
+
+            SyntaxTree tree = SyntaxFactory.ParseSyntaxTree(source);
+            string result = CodeAnalysisEditorUtility.PrintSyntaxTree(tree);
+
+            Assert.AreEqual(sourceTree, result);
+        }
     }
 }
--- Packages/UGF.Code.Analysis/Editor/CodeAnalysisEditorUtility.cs
+++ Packages/UGF.Code.Analysis/Editor/CodeAnalysisEditorUtility.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Editing;
@@ -32,6 +34,43 @@ namespace UGF.Code.Analysis.Editor
         public static SyntaxGenerator GetSyntaxGenerator()
         {
             return SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.CSharp);
+        }
+
+        public static string PrintSyntaxTree(SyntaxTree tree)
+        {
+            if (tree == null) throw new ArgumentNullException(nameof(tree));
+
+            var builder = new StringBuilder();
+
+            PrintSyntaxNode(builder, tree.GetRoot(), 0, "    ");
+
+            return builder.ToString();
+        }
+
+        private static void PrintSyntaxNode(StringBuilder builder, SyntaxNodeOrToken node, int depth, string tab)
+        {
+            builder.Append(string.Concat(Enumerable.Repeat(tab, depth)));
+            builder.Append($"{node.Kind()} {node.Span}");
+            builder.AppendLine();
+
+            foreach (SyntaxTrivia trivia in node.GetLeadingTrivia())
+            {
+                builder.Append(string.Concat(Enumerable.Repeat(tab, depth + 1)));
+                builder.Append($"Lead: {trivia.Kind()} {trivia.Span}");
+                builder.AppendLine();
+            }
+
+            foreach (SyntaxTrivia trivia in node.GetTrailingTrivia())
+            {
+                builder.Append(string.Concat(Enumerable.Repeat(tab, depth + 1)));
+                builder.Append($"Trail: {trivia.Kind()} {trivia.Span}");
+                builder.AppendLine();
+            }
+
+            foreach (SyntaxNodeOrToken nodeOrToken in node.ChildNodesAndTokens())
+            {
+                PrintSyntaxNode(builder, nodeOrToken, depth + 1, tab);
+            }
         }
     }
 }
